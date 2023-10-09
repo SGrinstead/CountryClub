f11a1e645bbaf188e6d700b41135bd527f924ca8
SGrinstead <seth.grinstead3299@gmail.com>
2023-10-09 12:26:32 -0700

    controller setup

---
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -0,0 +1,21 @@
+﻿using CountryClubAPI.DataAccess;
+using CountryClubAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.Metrics;
+
+namespace CountryClubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingsController : ControllerBase
+    {
+        private readonly CountryClubContext _context;
+
+        public BookingsController(CountryClubContext context)
+        {
+            _context = context;
+        }
+    }
+}
