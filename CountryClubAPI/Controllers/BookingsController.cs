ac217b50df60567bd34009f825fbe0cd98b5107f
SGrinstead <seth.grinstead3299@gmail.com>
2023-10-09 12:34:51 -0700

    create booking endpoint

--- CountryClubAPI/Controllers/BookingsController.cs
+++ CountryClubAPI/Controllers/BookingsController.cs
@@ -24,5 +24,20 @@ namespace CountryClubAPI.Controllers
 
             return new JsonResult(bookings);
         }
+
+        [HttpPost]
+        public IActionResult CreateBooking(Booking booking)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Bookings.Add(booking);
+            _context.SaveChanges();
+
+            Response.StatusCode = 201;
+            return new JsonResult(booking);
+        }
     }
 }
